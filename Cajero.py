800f9b73f918c9c7e83da2a933761446d7aad7fc
Jhon <hantoni577gmail.com>
2026-02-12 16:07:19 -0500

    Plantilla

---
+++ Cajero.py
@@ -0,0 +1,17 @@
+ BASE DE DATOS SIMPLE (Simulando una estructura relacional)
+# Cada clave es el número de cuenta y el valor son sus datos
+usuarios = {
+    "101": {"nombre": "Ana", "pin": "1111", "saldo": 1000},
+    "102": {"nombre": "Jhon", "pin": "2222", "saldo": 2000},
+    "103": {"nombre": "Brayan", "pin": "3333", "saldo": 3000}
+}
+
+print("--- SISTEMA MULTI-USUARIO ---")
+
+# 1. BUSCAR USUARIO
+
+
+# 2. VALIDACIÓN
+
+        
+# 3. TRANSACCIÓN
